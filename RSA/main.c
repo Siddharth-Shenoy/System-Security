2491da50daecb567cf280e7258b151cb7623498a
Siddharth-Shenoy <siddh.shenoy@gmail.com>
2018-02-27 00:37:25 +0530

    Update main.c

--- RSA/main.c
+++ RSA/main.c
@@ -1,114 +1,140 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <conio.h>
+#include <malloc.h>
 
-
-int findkeypair(int key,int p0,int p1);
-int RSAAlgoChar(char c,int power,int n);
-int gettotaldigits(int k);
-typedef struct RSAKeys
+typedef struct KEYS
 {
-	long enckey;
-	long deckey;
+	int key[2];
 }
 KEYS;
 
+int strval(char *str);
+int getTotalLength(int k);
+int generateKey(int k, int tn);
+int getMod(int val, int pow, int mod);
+char toupper(char c);
 int main()
 {
-  	int i = 0;
-  	int ei = 0;
-  	int di = 0;
-  	int k = 0;
-	int enc[128];
-  	int dec[128];
-  	char mainstring[128];
-	char newstr[128];
-  	char transmission[2048] =  "";
-	char declines[128];
-	long prime0;
-	long prime1;
-  	long n;
-	KEYS K;
-	char string[128];
-  	//char string[128] = "Hello world";
-	int buffersize = 128;
-	printf("Enter two prime numbers..\n");
-	scanf("%ld%ld",&prime0,&prime1);
-  	n = prime0 * prime1;
-	printf("Entered prime numbers: ( %ld , %ld )\n",prime0,prime1);
-	printf("Enter the encryption key:\n");
-	scanf("%ld",&K.enckey);
-	K.deckey = findkeypair(K.enckey,prime0,prime1);
-	printf("Key pair: %ld , %ld\n",K.enckey,K.deckey);
-  	printf("Enter a string to encrypt..\n");
-  	while ((getchar()) != '\n');
-	fgets(&mainstring,&buffersize,stdin);
-  	printf("Entered string: %s\n",mainstring);
-  	printf("String length: %d\n",strlen(mainstring));
-	for(i = 0; i < strlen(mainstring); i++)
-   	{
-	   	if(mainstring[i] == ' ')
-	    	{
-			continue;
-		}
-	   	else
-	    	{
-			newstr[k] = mainstring[i];
-		    	k++;
-		}
+	int p, q, tn, n, len;
+	char ch;
+	KEYS m_keys;
+	char m_input[128];
+	char m_inputcpy[128];
+	int m_enc[128];
+	int m_dec[128];
+	char encrypt[2048] = "";
+	char decryptx[128][12];
+	char decrypt[2048] = "";
+	int i, k, l;
+	int m_len, comb = 0, rem;
+	printf("Enter two prime numbers:\n");
+	scanf("%d%d", &p, &q);
+	n = p * q;
+	tn = (p - 1) * (q - 1);
+	printf("Enter your key:\n");
+	scanf("%d", &m_keys.key[0]);
+	m_keys.key[1] = generateKey(m_keys.key[0], tn);
+	printf("Key pair generated: %d %d\n", m_keys.key[0], m_keys.key[1]);
+	while ((ch = getchar()) != '\n');
+	fflush(stdin);
+	
+	printf("Enter a string(128 characters):\n");
+	fgets(m_inputcpy, sizeof(m_inputcpy), stdin);
+	k = 0;
+	for (i = 0; i < strlen(m_inputcpy); i++)
+	{
+		if (m_inputcpy[i] == ' ' || m_inputcpy[i] == ',' || m_inputcpy[i] == '?' || m_inputcpy[i] == ';') continue;
+		m_input[k] = toupper(m_inputcpy[i]);
+
+		k++;
 	}
-	printf("New string: %s\n",newstr);
-	for(i = 0; i < strlen(newstr); i++)
-    	{
-		enc[ei] = RSAAlgoChar(newstr[i],K.enckey,n);
-	    	printf("%d\n",enc[ei]);
-	    	sprintf(transmission,"%s%d",transmission,enc[ei]);
-	    	ei++;
+	m_input[k] = '\0';
+
+	printf("Entered string: %s\n", m_input);
+	for (i = 0; i < strlen(m_input); i++)
+	{
+		m_enc[i] = getMod((int)m_input[i], m_keys.key[0], n);
+		m_len = getTotalLength(m_enc[i]);
+
+		sprintf(encrypt, "%s%d",encrypt, m_enc[i]);
 	}
-  	//sprintf(transmission,"%s00%d",transmission,ei);
-  	printf("Transmission String:\n");
-  	printf("%s\n",transmission);
-  	printf("\n\n\nRECEPTION:\n");
-  	printf("Test Decryption for the letter 'A'\n");
-  	int testdec = RSAAlgoChar('A',K.deckey,n);
-  	printf("Total digits: %d\n",gettotaldigits(testdec));
-  	printf("Parsing transmission line..")
+	printf("Encrypted String: %s\n", encrypt);
+	len = getTotalLength(getMod((int)'A', m_keys.key[0], n));
+
+
+	k = 0;
+	for (i = 0; i < strlen(encrypt); i += len)
+	{
+		for (l = 0; l < len; l++)
+		{
+			decryptx[k][l] = encrypt[i + l];
+		}
+		decryptx[k][len] = '\0';
+		//printf("%s\n", decryptx[k]);
+		k++;
+	}
+
+	for (i = 0; i < k; i++)
+	{
+		m_dec[i] = strval(decryptx[i]);
+		decrypt[i] = (char)getMod(m_dec[i], m_keys.key[1], n);
+	}
+		
+	printf("Decrypted String: %s\n", decrypt);
+	_getch();
 	return 0;
 }
 
-int findkeypair(int key,int p0,int p1)
+int getMod(int val, int pow, int mod)
 {
-	int keyret = -1;
-	int totient = (p0-1)*(p1-1);
-	for(int i = 1; i < totient; i++)
+	int x = 1;
+	for (int i = 0; i < pow; i++)
 	{
-		if((key*i)%totient == 1)
+		x = (x*val) % mod;
+	}
+	return x;
+}
+int generateKey(int k, int tn)
+{
+	int ret = 1;
+	for (int i = 1; i < tn; i++)
+	{
+		if ((i*k) % tn == 1)
 		{
-			keyret = i;
+			ret = i;
 			break;
 		}
 	}
-	return keyret;
+	return ret;
 }
-
-int RSAAlgoChar(char c,int power,int n)
+int getTotalLength(int k)
 {
-	int value = (int)c;
-  	for(int i = 0; i < power; i++)
-    	{
-		value = (value*(int)c)%n;
+	int x = 0;
+	while (k != 0)
+	{
+		k /= 10;
+		x++;
 	}
-  	value = (value*(int)c)%n;
-	return value;
+	return x;
 }
-int gettotaldigits(int k)
+int strval(char *str)
 {
-  	int temp =  k;
-  	int total = 0;
-  	while(temp != 0)
-    	{
-		temp /= 10;
-	    	total++;
+	int result = 0;
+	for (int i = 0; i < strlen(str); i++)
+	{
+		result = result * 10 + ((int)str[i] - (int)'0');
 	}
-  	return total;
+	return result;
+}
+char toupper(char c)
+{
+	if (c == '\0') return c;
+	int val = (int)'a' - (int)'A';
+	int ret = c;
+	if (c >= 'a' && c <= 'z')
+	{
+		ret = c - val;
+	}
+	return (char)ret;
 }
